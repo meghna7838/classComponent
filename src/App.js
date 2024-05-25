ba3b267ce66665130f93384ee184205727c2a42f
meghna7838 <meghnatandon7838@gmail.com>
2024-05-25 22:17:59 +0530

    Debugging

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
 import React from "react";
 class App extends React.Component{
 
-  render(){
-    return <h1>Class component</h1>;
+  constructor(){
+    super();
+    this.state={
+      count:4,
+      vehicles:["Bus","Car","Bycycle","Motorcycle"],
+      favourite:"Car"
+    };
   }
-}
 
+  changeFavourite=()=>{
+    this.setState({favourite:"Motorcycle"});
+    this.setState((prevState)=>({count:prevState.count+1}));
+    console.log(this.state.favourite);
+  }
+
+  render(){
+    return   <><h2 onClick={this.changeFavourite}> Hello</h2>
+    <h2>{this.state.favourite}</h2>
+    <h2>{this.state.count}</h2>
+    </>
+  
+}
+}
 export default App;
